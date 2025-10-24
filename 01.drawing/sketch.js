b32914b2d3d494a06b9822cc4c48c42cff48a7ff
luvevelina <ebrusokaite14@yahoo.com>
2025-10-24 11:04:34 +0100

    Add files via upload

---
+++ 01.drawing/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  //redcircle
+  fill(255,0,0)
+  circle(50,50,80)
+  
+  fill(0,255,0)
+  circle(30,20,60)
+
+  fill(0,0,255)
+  circle(150,150,80)
+
+  textSize(30)
+  text("Hello World",200,200)
+}
