3eb4a9b6489328b595f367a8548863441e0c84a6
luvevelina <ebrusokaite14@yahoo.com>
2025-11-03 20:53:46 +0000

    Add files via upload

--- 03.multiples/sketch.js
+++ 03.multiples/sketch.js
@@ -1,7 +1,26 @@
+let theText= ["WAKEUP","EAT","SMILE","FAKEIT","WORK","STUDY","GRIND","FAKE IT","REPEAT","REPEAT", "REPEAT"];
+let word =theText[0];
+let i = 0;
+let index = 0;
+
 function setup() {
   createCanvas(400, 400);
+  frameRate(2)
 }
 
 function draw() {
-  background(220);
+  background(0, 0, 0);
+
+  //text
+  textSize(40)
+  fill(110, 255, 107)
+  textFont("Courier New")
+  text(word,130,200)
+
+  word=theText[i];
+  i=i+5
+  if(i>theText.length)
+  i=0
+
 }
+ console.log(i);
