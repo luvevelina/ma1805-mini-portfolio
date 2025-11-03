e66a0669f6cef4377b87ac2c5d90cba0beb19775
luvevelina <ebrusokaite14@yahoo.com>
2025-11-03 20:27:55 +0000

    Add files via upload

--- 02.movement/sketch.js
+++ 02.movement/sketch.js
@@ -1,15 +1,94 @@
-let name="Bob"
-let i=50
+let i = 0;
 
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
-  circle(i,150,i)
- i=i+1
+  background(144,238,144);
+  // L yellow circle base
+  fill(255,255,0)
+  circle(175,175,100)
 
-  text(name,200,150)
-  ext('hi', 50, 50);
+  // L left eye
+  fill(255,255,255)
+ circle(150,170,10)
+ fill(0,0,0)
+  circle(149,170,5)
+
+ //L right eye
+  fill(0,0,0)
+ circle(190,170,10)
+
+ // R yellow circle base
+  fill(255,255,0)
+  circle(230,175,100)
+
+ //R left eye
+  fill(255,255,255)
+ circle(200,170,10)
+ fill(0,0,0)
+ circle(199,170,5)
+ 
+ //R right eye
+  fill(255,255,255)
+ circle(250,170,10)
+ fill(0,0,0)
+ circle(249,170,5)
+
+ // R smile 
+ fill(0,0,0)        
+ curve(190,160,250,190,200,200,10,15)
+
+ //L frown
+ line(150,200,185,200)
+
+ // L eyebrow
+ line(142,155,160,150)
+
+ // R left eyebrow
+ line(190,155,210,155)
+
+ // R right eyebrow
+ line(245,155,265,155)
+
+ //moving text
+ 
+ textSize(32)
+ textStyle(BOLDITALIC)
+ text("fake it",i,25)
+
+ textSize(20)
+ textStyle(BOLDITALIC)
+ text("fake it",i,300)
+
+ textSize(50)
+ textStyle(BOLDITALIC)
+ text("fake it",50,i)
+
+ textSize(10)
+ textStyle(BOLDITALIC)
+ text("fake it",i,50)
+
+ textSize(20)
+ textStyle(BOLDITALIC)
+ text("fake it",600,i)
+
+ textSize(90)
+ textStyle(BOLDITALIC)
+ text("fake it",i,i)
+
+ textSize(20)
+ textStyle(BOLDITALIC)
+ text("fake it",i,i+50)
+
+ 
+ textSize(20)
+ textStyle(BOLDITALIC)
+ text("fake it",i+100,i+30)
+
+ //increment i
+  i = i+1;
+
 }
+
